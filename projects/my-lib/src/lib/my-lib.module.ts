92c92d50cf2e6689cb268f862532bff7ddb24f72
Jorge Herrera <jorherrera@bgeneral.com>
2023-06-09 10:13:13 -0500

    [Fix]: Linting packages

--- projects/my-lib/src/lib/my-lib.module.ts
+++ projects/my-lib/src/lib/my-lib.module.ts
@@ -2,11 +2,12 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { MyLibRoutingModule } from './my-lib-routing.module';
 import { CommonModule } from '@angular/common';
 import { RickMortyService } from './services/rick-morty.service';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  imports: [CommonModule, MyLibRoutingModule],
+  imports: [CommonModule, MyLibRoutingModule, IonicModule],
   providers: [RickMortyService],
 })
 export class MyLibModule {}
